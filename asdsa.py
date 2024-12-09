6a66b453cba037fe18b9bd47acfb8c4b3f3ead78
ernests1134 <ernests.samorukovs@gmail.com>
2024-12-09 12:49:17 +0200

    Add files via upload

---
+++ asdsa.py
@@ -0,0 +1,23 @@
+width = 10
+
+height = 5
+
+for i in range(height):
+
+print('*' * width)
+
+side = 5
+
+for i in range(side):
+
+print('*' * side)
+
+levels = 5
+
+for i in range(1, levels + 1):
+
+spaces = ' ' * (levels - i)
+
+stars = '*' (2 i - 1)
+
+print(spaces + stars)
